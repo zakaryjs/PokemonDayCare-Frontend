1c6e1f3ea73faf63b4ae7f03a9e7a68dba77a0d3
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-11-30 14:43:12 +0800

    created navbar component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import AccountPortal from './pages/AccountPortal';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/portal' element={<AccountPortal />} />
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>
 );
