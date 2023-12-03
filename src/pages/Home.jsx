d2e99d6953d9c43566535dbc6ad59767ee09887d
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-03 19:15:25 +0800

    added green text highlights

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,12 +15,27 @@ export default function Home() {
                 <img src={require('../images/Zak-Logo-BG-removed.png')} alt='logo' height={180} width={320} />
             </div>
             <NavBar />
-            <p className='margin-top-extra'>The best daycare in the region!</p>
-            <p className='margin-top'>We provide services that fit all trainers needs:</p>
-            <div>
-                <p className='margin-top'>Real-world experience gain</p>
-                <p className='margin-top'>Specialised competitive training</p>
-                <p className='margin-top'>1/4000 -- breeding services</p>
+            <div className='margin-top-extra jumbo-text'>
+                <span className='margin-top-extra'>The </span>
+                <span className='margin-top-extra green-text'>best daycare </span>
+                <span className='margin-top-extra'>in the region!</span>
+            </div>
+            <div className='margin-top jumbo-text'>
+                <span className='margin-top'>We provide services that </span>
+                <span className='margin-top green-text'>fit all trainers </span>
+                <span className='margin-top'>needs:</span>
+            </div>
+            <div className='margin-top jumbo-text'>
+                <span className='margin-top green-text'>Real-world </span>
+                <span className='margin-top'>experience gain</span>
+            </div>
+            <div className='margin-top jumbo-text'>
+                <span className='margin-top green-text'>Specialised </span>
+                <span className='margin-top'>competitive training</span>
+            </div>
+            <div className='margin-top jumbo-text'>
+                <span className='margin-top green-text'>1/4000 </span>
+                <span className='margin-top'>-- breeding services</span>
             </div>
             <div className='text-center margin-top-extra'>
                 <Button href="/about" variant='success'>About</Button>
