26c7f58dc1d852c6e64e3e4ecf4b5341bf62cb00
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-04 14:16:57 +0800

    added background particles to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,4 @@
+import BackgroundParticles from '../components/BackgroundParticles';
 import NavBar from '../components/NavBar';
 
 
@@ -6,6 +7,7 @@ export default function About() {
 
     return (
         <>
+        <BackgroundParticles />
             <div className='image'>
                 <img src={require('../images/Zak-Logo-BG-removed.png')} alt='logo' height={180} width={320} />
             </div>
