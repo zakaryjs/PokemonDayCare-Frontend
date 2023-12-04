e0db320b56c4eff6575a3440f6e55c29b8ce513f
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-04 14:35:11 +0800

    added body text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import BackgroundParticles from '../components/BackgroundParticles';
 import NavBar from '../components/NavBar';
+import '../styles/About.css'
 
 
 export default function About() {
@@ -14,6 +15,13 @@ export default function About() {
             <NavBar />
             <div className='image margin-top-extra'>
                 <img src={require('../images/Day_Care_Unova.png')} alt='daycare' height={180} width={320} />
+            </div>
+            <div>
+                <p className='margin-top p-margins'>The lovely Pokemon Day Care has been running for many years.</p>
+                <p className='margin-top p-margins'>Both Grant and Martha have enjoyed every year they have
+                spent servicing the community through caring for
+                Pokemon.
+                </p>
             </div>
         </>
     )
