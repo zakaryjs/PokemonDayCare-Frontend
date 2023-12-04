f8f4814181bcb4f6d6a403fbeb94e05ffdfad7fc
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-04 14:47:46 +0800

    added more body text and footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import BackgroundParticles from '../components/BackgroundParticles';
 import NavBar from '../components/NavBar';
 import '../styles/About.css'
+import { CFooter } from '@coreui/react';
 
 
 export default function About() {
@@ -23,6 +24,22 @@ export default function About() {
                 Pokemon.
                 </p>
             </div>
+            <div className='image'>
+                <img className='margin-top' src={require('../images/oldcouple-gen3.png')} alt='old couple' height={160} width={160} />
+            </div>
+            <div>
+                <p className='margin-top p-margins'>With their strong expertise in the field of Pokemon care,
+                there is no Pokemon that is too tough for them.
+                </p>
+                <p className='margin-top p-margins'>Regardless of the service you choose, be assured that you have
+                made the right choice!
+                </p>
+            </div>
+            <CFooter className='fixed-bottom centred margin-top'>
+            <div>
+                <span>&copy; 2023 Zakary Sutherland</span>
+            </div>
+            </CFooter>
         </>
     )
 }
